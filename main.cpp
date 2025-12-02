2acf162c5d08f6c1bf9af160734d4dba4a2b3c65
komalacharyaa <94459607+komalacharyaa@users.noreply.github.com>
2025-12-02 18:42:19 -0500

    add includes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 torch::Tensor forward(torch::Tensor q, torch::Tensor k, torch::Tensor v);
 
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward", torch::wrap_pybind_function(forward), "forward");
 }
