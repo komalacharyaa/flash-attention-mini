7f351f966230bd9d8715fe4d63a8bd533a763e7f
komalacharyaa <94459607+komalacharyaa@users.noreply.github.com>
2025-12-02 18:42:54 -0500

    add includes

--- flash.cu
+++ flash.cu
@@ -6,6 +6,7 @@ __global__
 void forward_kernel(const float* Q, const float* K, const float* V, const int N, const int d,
                     const int Tc, const int Tr, const int Bc, const int Br, const float softmax_scale,
                     float* l, float *m, float* O) {
+
     int tx = threadIdx.x;
     int bx = blockIdx.x; int by = blockIdx.y;  // batch and head index
 
